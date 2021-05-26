52c6d2e2e608f8d9fc3a202cf85b31896b425add
sorkwhwo <84855628+sorkwhwo@users.noreply.github.com>
2021-05-26 22:52:22 +0900

    javaTest2

---
+++ Dog.java
@@ -0,0 +1,12 @@
+package onlypractice3;
+
+public class Dog {
+
+	static void a() {
+		System.out.println("aaaa"+name);
+	}
+	static String name;
+	public String toString() {
+		return null;
+	}
+}
---
+++ DogTest.java
@@ -0,0 +1,14 @@
+package onlypractice3;
+
+public class DogTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Dog myDog = new Dog();
+		System.out.println(myDog);
+		System.out.println(myDog.toString());
+	
+	
+	}
+}
